ab850639885d3f4ec6584a96e538ba70efca930c
mac <v.kotelnikovdidi@gamil.com>
2024-01-14 19:04:40 +0200

    test 2

--- Keyboards/Keyboards.py
+++ Keyboards/Keyboards.py
@@ -1,39 +1,14 @@
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, KeyboardButtonPollType,InlineKeyboardButton, InlineKeyboardMarkup, Message
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from aiogram import Bot
 from aiogram.types import BotCommand
-from Telegram_bot.answer_data.answer_bot_data import COMANDS_START_MENU
 
-
-
-
-button_OK = KeyboardButton(text="Обязательно приду на футбол 🖐")
-button_NO = KeyboardButton(text="Занят.Бухаю 🍻")
-
-
-
-url_button_1 = InlineKeyboardButton(
-    text='Курс "Телеграм-боты на Python и AIOgram"',
-    url='https://stepik.org/120924'
-    )
-
-url_button_2 = InlineKeyboardButton(
-    text='Документация Telegram Bot API',
-    url='https://core.telegram.org/bots/api')
-
-url_button_3 = InlineKeyboardButton(
-    text="BMV X5 2015",
-    url="https://qna.habr.com/q/1242714",
-
-)
-keyboard = InlineKeyboardMarkup(
-    inline_keyboard=[[url_button_1],
-                     [url_button_2],
-                     [url_button_3]]
-    )
-
-
+from Bookbot.lexicon.lexicon import LEXICON_COMMANDS
 
 
 # Функция для настройки кнопки Menu бота
-
+async def set_main_menu(bot: Bot):
+    main_menu_commands = [BotCommand(
+        command=command,
+        description=description
+    ) for command,
+        description in LEXICON_COMMANDS.items()]
+    await bot.set_my_commands(main_menu_commands)
--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,43 +1,11 @@
-
-from aiogram import Router, F
-from ..answer_data.answer_bot_data import ANSWER
-from aiogram.filters import CommandStart
-from aiogram.types import (Message,
-                           ReplyKeyboardRemove)
-
+from aiogram import Router
+from aiogram.types import Message
 
 router = Router()
 
 
-# @router.message()
-# async def send_echo(message: Message):
-#     try:
-#         await message.send_copy(chat_id=message.chat.id)
-#     except TypeError:
-#         await message.reply(text=answer['no_echo'])
-
-
-# @router.message(CommandStart())
-# async def process_start_command(message: Message):
-#     await message.answer(
-#         text='Футбол СБ на 10:00. Будешь?',
-#         reply_markup=Kb
-#     )
-#
-#
-# # Этот хэндлер будет срабатывать на ответ "Собак 🦮" и удалять клавиатуру
-# @router.message(F.text == 'Обязательно приду на футбол 🖐')
-# async def process_football_yes_answer(message: Message):
-#     await message.answer(
-#         text='КРАСАВА, НЕ ОПАЗДЫВАЙ!',
-#         #reply_markup=ReplyKeyboardRemove()
-#     )
-#
-#
-# @router.message(F.text == 'Занят.Бухаю 🍻')
-# async def process_football_no_answer(message: Message):
-#     await message.answer(
-#         text='Понимаю, 3-й тайм тоже важно.\n'
-#              'Рожкову - Привет!',
-#         #reply_markup=ReplyKeyboardRemove()
-#     )
+# Этот хэндлер будет реагировать на любые сообщения пользователя,
+# не предусмотренные логикой работы бота
+@router.message()
+async def send_echo(message: Message):
+    await message.answer(f'Это эхо! {message.text}')
